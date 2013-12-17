90bc09c067aa63159e266b14b797beec16c2abce
arts-unimelb <arts-webmaster@unimelb.edu.au>
2013-12-17 11:04:23 +1100

    Fix the font issue by javascript

---
+++ js/local.js
@@ -0,0 +1,19 @@
+(function ($) {
+  Drupal.behaviors.unimelb = {
+    attach: function(context, settings) {
+
+			// Used by fontdesk
+			WebFontConfig = { fontdeck: { id: '40692' } };
+
+			(function() {
+  			var wf = document.createElement('script');
+  			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  			wf.type = 'text/javascript';
+  			wf.async = 'true';
+  			var s = document.getElementsByTagName('script')[0];
+  			s.parentNode.insertBefore(wf, s);
+			})();
+
+		}
+	}
+})(jQuery);
